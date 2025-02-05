2ead9851213adbd644f39c425787ee7c41c6d1de
Rahul Vishwakarma <rahul.vishwakarma@watchyourhealth.com>
2025-02-05 11:30:14 +0530

    commit New

---
+++ WebViewTestJS/TableInSideTable/StackInsideTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  StackInsideTableViewCell.swift
+//  WebViewTestJS
+//
+//  Created by Rahul Vishwakarma on 03/01/25.
+//
+
+import UIKit
+
+class StackInsideTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ WebViewTestJS/TableInSideTable/StackInsideViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StackInsideViewController.swift
+//  WebViewTestJS
+//
+//  Created by Rahul Vishwakarma on 03/01/25.
+//
+
+import UIKit
+
+class StackInsideViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
